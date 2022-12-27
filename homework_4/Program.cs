8c1e9347baf26dde3e6800ad4ce5bf0e5d67da6f
Svetlana <svkos.76@mail.ru>
2022-12-27 13:34:07 +0300

    homework 4

--- homework_4/Program.cs
+++ homework_4/Program.cs
@@ -5,7 +5,19 @@
 
 // 2, 4 -> 16
 
+Console.WriteLine("Введите число A");
+int numA=Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Введите число B");
+int numB=Convert.ToInt32(Console.ReadLine());
+
+int i=1;
+while (numB>0)
+{
+   i*=numA;
+   numB=numB-1;
+}
+Console.WriteLine(i);
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
@@ -16,6 +28,16 @@
 // 9012 -> 12
 
 
+Console.WriteLine("Введите число");
+int numbers1=Convert.ToInt32(Console.ReadLine());
+string num1=Convert.ToString(numbers);
+int sum1=0;
+for (int ind1=0; ind1<num1.Length; ind1++)
+{
+   sum1=sum1+Convert.ToInt32(num1[ind1].ToString());
+}
+         
+Console.WriteLine(sum);
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
@@ -23,3 +45,14 @@
 
 // 6, 1, 33 -> [6, 1, 33]
 
+int [] Array8 ()
+{
+   int [] array=new int [8];
+   for (i=0; i<array.Length; i++)
+   {
+      Console.WriteLine("Введите элемент массива");
+      array[i]=Convert.ToInt32(Console.ReadLine());
+   }
+   return array;
+}
+Console.WriteLine(string.Join(",", Array8()));
